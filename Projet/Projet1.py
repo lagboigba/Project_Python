d0d6c88e8bfef683c6efe93f2ac7cc6baa4f8d60
OlympieSuquet <olympie.suquet@laposte.net>
2019-02-12 17:00:47 +0100

    modif

--- Projet/Projet1.py
+++ Projet/Projet1.py
@@ -3,13 +3,19 @@ import csv
 
 def readFileCSV(file):
     ponderation=[]
+    data=[]
+    result=[]
+    data.append('truc')
     with open(file, newline='', encoding='utf-8') as f:
         reader = csv.reader(f, delimiter=';')
         for row in reader:
             if row[0]=='':
                 for i in range(1,len(row)-1):
                     ponderation.append(row[i])
-    return ponderation        
+            else:
+                
+    return ponderation, data, result   
             
-ponderation = readFileCSV('criteres.csv')
+ponderation, data, result = readFileCSV('criteres.csv')
 print(ponderation)
+print(data)
