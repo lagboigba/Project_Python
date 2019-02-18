ac5929f15cd359ef236f5700590c86ce070e7d4b
OlympieSuquet <olympie.suquet@laposte.net>
2019-02-18 09:20:23 +0100

    fonctions lecture ok

--- Projet/Projet1.py
+++ Projet/Projet1.py
@@ -1,7 +1,7 @@
 import csv
+import numpy as np
 
-
-def readFileCSV(file):
+def readFileCSVData(file):
     """
     Lit un fichier CSV
     :param file:
@@ -11,20 +11,36 @@ def readFileCSV(file):
     """
     ponderation=[]
     data=[]
-    result=[]
-    data.append('truc')
+    result=[]#derniere colonne
     with open(file, newline='', encoding='utf-8') as f:
         reader = csv.reader(f, delimiter=';')
         for row in reader:
-            if row[0]=='':
+            if row[0]=='': #lecture des ponderations
                 for i in range(1,len(row)-1):
                     ponderation.append(row[i])
-            else:
-                print ('Haram')
+            else:#lecture des donnees et des resultats
+            #donnees contient les notes et result la moyenne
+                temp=[]
+                for i in range(1,len(row)-1):
+                    temp.append(row[i])
+                dicodata={row[0]:temp}
+                dicoresult={row[0]:row[len(row)-1]}
+                data.append(dicodata)
+                result.append(dicoresult)
                 
     return ponderation, data, result   
 
+ponderation, data, result = readFileCSVData('criteres.csv')
 
-ponderation, data, result = readFileCSV('criteres.csv')
-print(ponderation)
-print(data)
+def readFileCSVInt(file):
+    intervalle=[]
+    with open(file, newline='', encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter=';')
+        for row in reader:
+            dico={row[0]:[row[1],row[2]]}
+            intervalle.append(dico)
+    return intervalle
+    
+intervalle = readFileCSVInt('intervalles.csv')
+print(len(intervalle))
+
